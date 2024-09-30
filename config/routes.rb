032b885be2160559b12459a82ac05b75c04c9643
Ashley George <ashgeo1986@gmail.com>
2024-09-30 15:28:53 +0100

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'categories', to: 'categories#index'
+  get 'categories/new', to: 'categories#new'
+  get 'categories/:id', to: 'categories#show', as: :category
+  post 'categories', to: 'categories#create'
+  delete 'categories/:id', to: 'categories#destroy'
 end
